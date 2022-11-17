4c0c40c6e73308ff51c0e876beea922b12fd19ce
LethiciaDuncan <lduncan@student.neumont.edu>
2022-11-17 02:20:21 -0700

    Login and register pages basic design

---
+++ Galleria/Galleria/Areas/Identity/Data/GalleriaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Galleria.Data;
+
+public class GalleriaContext : IdentityDbContext<IdentityUser>
+{
+    public GalleriaContext(DbContextOptions<GalleriaContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
