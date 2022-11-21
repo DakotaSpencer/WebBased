7b80aeccfa7703ed298833842749c41f4fcee7f5
JeremieCius <jeremiecius@gmail.com>
2022-11-21 09:38:22 -0700

    CommissionDAL in progress

---
+++ Galleria/Galleria/Data/CommissionDAL.cs
@@ -0,0 +1,36 @@
+﻿using Galleria.Models; 
+using Galleria.Interfaces;
+
+namespace Galleria.Data
+{
+    public class CommissionDAL
+    {
+        private CommissionContext db;
+
+        public CommissionDAL(CommissionContext db)
+        {
+            this.db = db;
+        }
+
+        public Commission GetCommission(int? id)
+        {
+            return db.Commissions.FirstOrDefault(commission => commission.CommissionId == id);
+        }
+
+        public IEnumerable<Commission> Search (string strCommissionSearch)
+        {
+            List<Commission> foundCommissions = new List<Commission>();
+
+            foreach (var commission in db.Commissions)
+            {
+                if (commission.ArtistName.ToUpper().Contains(strCommissionSearch.ToUpper()) ||
+                    commission.CommissionName.ToUpper().Contains(strCommissionSearch.ToUpper()))
+                {
+                    foundCommissions.Add(commission);
+                }
+            }
+
+            return foundCommissions;
+        }
+    }
+}
