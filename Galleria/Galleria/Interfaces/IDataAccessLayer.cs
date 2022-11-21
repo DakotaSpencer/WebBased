7418df6a0841c592af82ca7debe03460e2ab8bb3
JeremieCius <jeremiecius@gmail.com>
2022-11-21 08:47:40 -0700

    IDataAccessLayer for Commissions done

--- Galleria/Galleria/Interfaces/IDataAccessLayer.cs
+++ Galleria/Galleria/Interfaces/IDataAccessLayer.cs
@@ -1,7 +1,19 @@
-﻿namespace Galleria.Interfaces
+﻿using Galleria.Models;
+
+namespace Galleria.Interfaces
 {
     public interface IDataAccessLayer
     {
+        public IEnumerable<Commission> GetCommissions();
 
+        public IEnumerable<Commission> SearchCommissions(string strSearchCommission);
+
+        public IEnumerable<Commission> FilterCommissions(string artistName, string commissionName, DateTime datePublished);
+
+        void AddCommission(Commission commission);
+
+        void DeleteCommission(int? id);
+
+        void GetCommission(int? id);
     }
 }
